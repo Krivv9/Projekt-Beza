f53873614e482017a972606fce92c000587d37d1
Krivv9 <kamil.racki@gmail.com>
2020-09-06 12:52:49 +0200

    init commit

---
+++ src/main/java/pl/coderslab/superprojekt/SuperprojektApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.superprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/superprojekt/SuperprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.superprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
