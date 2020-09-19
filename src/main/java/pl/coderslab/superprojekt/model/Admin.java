725d4de231d3dd75fa9b409d554cb7834e2b2ef3
Krivv9 <kamil.racki@gmail.com>
2020-09-19 16:50:47 +0200

    update models@Id                  @GeneratedValue(strategy = GenerationType.IDENTITY)                  private Long id;
                     private String firstName;
                     private String lastName;
                     private Integer phoneNumber;
                     private String password;

--- src/main/java/pl/coderslab/superprojekt/model/Admin.java
+++ src/main/java/pl/coderslab/superprojekt/model/Admin.java
@@ -1,4 +1,15 @@
 package pl.coderslab.superprojekt.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
 }
